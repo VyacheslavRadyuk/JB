ad66515b2d71538a7964a9cf02beda7d8425771f
Vyacheslav Radyuk <vyacheslav.radyuk@jetbi.com>
2021-12-10 20:33:20 +0300

    corrected

--- force-app/main/default/aura/sergey/sergeyController.js
+++ force-app/main/default/aura/sergey/sergeyController.js
@@ -50,5 +50,43 @@
             }
         });
         $A.enqueueAction(action);
+    },
+
+    handleDeleteRecord: function(component, event, helper) {
+        let toBeDeletedRowIndex = event.getSource().get("v.name");
+        let objectName = component.get('v.selectedValue');
+        let action = component.get("c.deleteRecord");
+        action.setParams({
+            recordToDeleteId: toBeDeletedRowIndex,
+            name: objectName
+        });
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {   
+                let refreshData = component.get('v.data');
+                let j = 0;
+                while (j < refreshData.length) {               
+                    if(refreshData[j].Id == toBeDeletedRowIndex) {
+                        refreshData.splice(j, 1);
+                    }else {
+                        j++;
+                    }
+                }
+                component.set('v.data', refreshData);
+            }                    
+            else if (state === "ERROR") {
+                let errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } 
+                else {
+                    console.log("Unknown Error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
