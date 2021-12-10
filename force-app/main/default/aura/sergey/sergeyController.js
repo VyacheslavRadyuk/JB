d7fc91f0da5279d622c991078b32f03a5cefbe5c
Vyacheslav Radyuk <vyacheslav.radyuk@jetbi.com>
2021-12-10 15:27:38 +0300

    Add 4 and 5 tasks from 3rd iteration

--- force-app/main/default/aura/sergey/sergeyController.js
+++ force-app/main/default/aura/sergey/sergeyController.js
@@ -1,7 +1,54 @@
 ({
-    myAction : function(component, event, helper) {
-        component.get('v.attribute');
-        component.set('v.attribute', peredat);
-        var aaa = component()
+    init: function(component, event, helper) {
+        let action = component.get("c.getObjectName");
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {           
+                let allValues = response.getReturnValue();
+                component.set("v.options", allValues);
+            }                    
+            else if (state === "ERROR") {
+                let errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } 
+                else {
+                    console.log("Unknown Error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
+     },
+
+    selectObject: function(component, event, helper) {
+        let objectName = component.get('v.selectedValue');
+        let action = component.get("c.getObjectByName");
+        action.setParams({
+            name: objectName
+        });
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {           
+                let allValues = response.getReturnValue();
+                component.set("v.data", allValues);
+                console.log(component.get('v.data'));
+            }                    
+            else if (state === "ERROR") {
+                let errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } 
+                else {
+                    console.log("Unknown Error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
---
+++ force-app/main/default/lwc/flightCreation/flightCreation.js
@@ -0,0 +1,108 @@
+import getTourist from '@salesforce/apex/FlightCreationController.getTourists';
+import getTrip from '@salesforce/apex/FlightCreationController.getTripsByCountryName';
+import getCountries from '@salesforce/apex/FlightCreationController.getCountries';
+import createFlightByTouristAndTripIds from '@salesforce/apex/FlightCreationController.createFlightByTouristAndTripIds';
+import { LightningElement, track, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class FlightCreation extends LightningElement {
+
+    @track touristRecords = [];
+    @track tripRecords = [];
+    @track countryRecords = [];
+    selectedTripName;
+    selectedTouristName;
+    selectedCountryName;
+    flightName;
+
+    @wire(getTourist)
+    wiredTouristRecords({ error, data }) {
+        if (data) {
+            this.error = undefined;
+            console.log(data);
+            this.touristRecords = JSON.parse(JSON.stringify(data));
+            console.log(this.touristRecords);
+        } else if (error) {
+            this.error = error;
+            this.touristRecords = undefined;
+        }
+    }
+
+    @wire(getCountries)
+    wiredCountryRecords({ error, data }) {
+        if (data) {
+            this.error = undefined;
+            this.countryRecords = JSON.parse(JSON.stringify(data));
+            console.log(this.countryRecords);
+        } else if (error) {
+            this.error = error;
+            this.countryRecords = undefined;
+        }
+    }
+
+    fetchTripByCounry() {
+        getTrip({
+            countryName : this.selectedCountryName
+        })
+        .then((result) => {
+            this.tripRecords = JSON.parse(JSON.stringify(result));
+            console.log('@@@ ' + this.tripRecords);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    handleTrip(event) {
+        const boxes = this.template.querySelectorAll('lightning-input');
+        boxes.forEach(box => box.checked = event.target.name === box.name);
+        this.selectedTripName = event.target.name;       
+        console.log(this.selectedTripName);
+    }
+
+    handleTourist(event) {
+        const boxes = this.template.querySelectorAll('lightning-input');
+        boxes.forEach(box => box.checked = event.target.name === box.name);
+        this.selectedTouristName = event.target.name;       
+        console.log(this.selectedTouristName);
+    }
+
+    handleCountry(event) {
+        const boxes = this.template.querySelectorAll('lightning-input');
+        boxes.forEach(box => box.checked = event.target.name === box.name);
+        this.selectedCountryName = event.target.name; 
+        console.log(this.selectedCountryName);
+        this.fetchTripByCounry();
+    }
+
+    handleClick(event) {
+        this.createFlight();
+    }
+
+    createFlight() {
+        createFlightByTouristAndTripIds({
+            touristId: this.selectedTouristName,
+            tripId : this.selectedTripName,
+            name : this.flightName  
+        })
+        .then((result) => {
+            this.showToast();
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Success!',
+            message: 'The flight has been created',
+            type: 'success'
+        });
+        this.dispatchEvent(event);
+    }
+
+    handleInputChange(event) {
+        this.flightName = event.detail.value;
+    }
+}
